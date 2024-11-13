bd2b1d81428e6b7a0ba40b5c34d0b32e5f730859
Jim Verducci <68206824+jim-verducci@users.noreply.github.com>
2024-11-12 16:27:36 -0800

    Release 1.0.0-rc2: Fix refresh token error edge case; update dependencies (#3)

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,19 +1,2 @@
-export class WristbandError extends Error {
-  private error: string;
-  private errorDescription?: string;
-
-  constructor(error: string, errorDescription?: string) {
-    super(error);
-    this.name = 'WristbandError';
-    this.error = error;
-    this.errorDescription = errorDescription;
-  }
-
-  getError(): string {
-    return this.error;
-  }
-
-  getErrorDescription(): string | undefined {
-    return this.errorDescription;
-  }
-}
+export { FetchError } from './fetch-error';
+export { WristbandError } from './wristband-error';
